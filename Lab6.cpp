63eb102ea4ed0dd30e5414350ee5b4bf3f7145b3
Matthew Alves <45500132+Alvesm3@users.noreply.github.com>
2019-08-06 16:51:11 -0400

    Created Lab6.cpp

---
+++ Lab6.cpp
@@ -0,0 +1,345 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+//creates player struct
+struct player{
+    string first, last;
+    int year[5];
+    int numWin;
+    player *next;
+    player *prev;
+};
+//create pointers that will be maniuplated 
+player *head = NULL;
+player *ptr = NULL; 
+player *sort = NULL; 
+
+//menu for user 
+char menu(){
+    char choice;
+    cout << "Menu\n";
+    cout << "1. Display the players\n";
+    cout << "2. Add a player\n";
+    cout << "3. Search(name, year, # of wins)\n";
+    cout << "4. Delete a player\n";
+    cout << "5. Exit\n";
+    cin >> choice;
+    
+    return choice;
+} 
+
+void createplayer(string first, string last, int year){ 
+//creating a new player entry
+  player *nptr = new player;
+//setting up variable types first name, last name, year, and number of wins
+  nptr->first = first;
+  nptr->last = last;
+  nptr->year[0] = year;
+  nptr->numWin = 1;
+//Checking the first entry 
+  if(head == 0){
+    head = nptr;
+    nptr->next = nptr;
+    nptr->prev = nptr;
+  }else{
+    //linking the doubly linked list
+    nptr->prev = ptr;
+    nptr->next = ptr->next;
+    ptr->next->prev = nptr;
+    ptr->next = nptr;
+}
+}
+
+void populate(){
+
+  int element = 0;
+  ifstream infile("Lab6_QB_List.txt");//function to get information from text file
+  string last; 
+  string first;
+  int year;
+//this algorithm will popualte the list
+  while(!infile.eof()) {
+    element++;
+    infile >> first >> last >> year;
+   //if the first element 
+    if(element == 1){
+      ptr = head;
+      createplayer(first, last, year);
+    }else{ 
+    //if the next element
+    //intialize counters and indexes
+      int count = 0;
+      int index = 0;
+      sort = 0;// pointer value intialized to zero
+      player *temp; 
+      temp = head;//set temp equal to the beginnin of the list 
+      //loop till all the elements have been inserted
+      while(!count){ 
+        if(last[index] > temp->last[index]){
+          if((temp->next == sort) || (temp->next == head)){  
+            ptr = temp;
+            createplayer(first, last, year);
+            count++; //ends loop once reaches limit
+          }else{
+            sort = temp;
+            temp = temp->next;
+            index = 0;
+          }
+        }else if(last[index] < temp->last[index]){
+          if(temp->prev == sort){
+            ptr = temp->prev;
+            createplayer(first, last, year);
+            count++; 
+          }else{
+            sort = temp;
+            temp = temp->prev;
+            index = 0;
+          }
+        }else{
+          index++;
+        }
+      if(last == temp->last){
+        if(first == temp->first){ 
+          (temp->numWin)++;
+          temp->year[(temp->numWin)-1] = year; 
+          count++;
+}
+}
+}if(temp->last[0] < head->last[0])
+        head = temp;
+}if(element == 19)
+      break;
+}
+}
+
+void add(){
+    
+  int found = 0; //check to see is player is in list already
+  cout << "Please enter in a name and year\n";
+  string first, last;
+  int year;
+  cin >> first >> last >> year;
+//loop through entire list
+for(player *temp = head->next ; temp != head ; temp = temp->next){ 
+    if((first == temp->first) && (last == temp->last)){//if the name entered equals a name in the list 
+      (temp->numWin)++; //incremenet wins
+      temp->year[(temp->numWin)-1] = year; //add year to player
+      found++;//increment found
+    }
+  }if(!found){//if player is not found in list, then add him 
+    ptr = head->prev;
+    createplayer(first, last, year);
+  }
+}
+
+void search(){
+  cout << "1. search by name\n";
+  cout << "2. search by year\n";
+  cout << "3. search by number of wins\n";
+  cout << "4. exit\n";
+  char choice;
+  cin >>choice;
+//exit
+  if(choice == '4')  
+    return;
+//Searching for name
+else if(choice == '1'){
+    cout << "Enter the player's name" << endl;
+    string first, last;
+    cin >> first >> last;
+//if name entererd equals first and last (first entry)
+if(head->first == first){
+    if(head->last == last){
+        cout << head->first <<" "<< head->last << " ";//print name
+        
+for(int i = 0; i < head->numWin; i++){
+          cout << head->year[i] << " ";//print year that player won superbowl
+} 
+cout << endl;
+}
+}else{
+
+      int found = 0;//if element is found
+
+      player *temp;
+      for(temp = head->next; temp != head; temp = temp->next){//iterate through list 
+      //if name entered equals first and last (all other case)
+        if(temp->first == first){
+          if(temp->last == last) 
+
+           cout << "Player: " << temp->first <<" "<< temp->last << " ";//print name
+           for(int i = 0; i < temp->numWin; i++){
+              cout << temp->year[i] << " ";//print year they won superbowl
+           }cout << endl;
+            found++;
+        }
+      
+    }
+    if(!found)  
+        cout << "Player not found" << endl;
+    }
+    return;
+//search for year
+}else if(choice == '2'){
+
+    cout << "Enter a year between 2000 and 2018" << endl;
+    int year;
+    cin >> year;
+
+    int found = 0; 
+    for(int i = 0; i < head->numWin; i++){//loops through years  
+    //checks to see if year entered equals year in list
+        if(head->year[i] == year){
+        cout << "Player: " << head->first <<" "<< head->last << " " << endl;//print name
+        found++; 
+      }
+    }
+    if(!found){
+      player *temp;
+      //other cases
+      for(temp = head->next; temp != head; temp = temp->next){//loops through list
+        for(int i = 0; i < temp->numWin; i++){  
+          if(temp->year[i] == year){//if year is found
+            cout << "Player: " << temp->first <<" "<< temp->last << " " << endl;//print out name
+            found++;
+          }
+        }
+}
+if(!found) 
+        cout << "The player was not found" << endl;
+    }
+    return;
+  }
+//search for number of wins
+else if(choice == '3'){
+
+    cout << "Enter players number of wins (Max is 5)" << endl;
+    int numWin;
+    cin >> numWin;
+//for first element
+    if(head->numWin == numWin){//checks to see number entered equals number of wins in list
+      cout << "Player: " << head->first <<" "<< head->last << " " << endl;//print name
+}else{
+      int found = 0; 
+//all other cases
+      player *temp;
+      for(temp = head->next; temp != head; temp = temp->next){//loop through list 
+        if(temp->numWin == numWin){ //check to see if number entered equals numver of wins in list
+          cout << "Player: " << temp->first <<" "<< temp->last << " " << endl;//print name
+          found++; 
+          }
+      }
+        if(!found)  
+        cout << "The player was not found" << endl;
+    
+    return;
+  }
+}
+
+}
+
+void deleteplayer(){
+
+  cout << "Please enter a name\n" << endl;
+  string first, last;
+  cin >> first >> last;
+
+  player *del_ptr;//pointer that selects which element will be deleted 
+  int found = 0;
+//if name entered equals name in list
+  if(head->first == first){
+        if(head->last == last){ 
+            del_ptr = head;//pointer to be deleted equal to head
+            head = head->next; //set head equal to head next
+            found++; 
+}
+}
+else{
+    player *temp;
+    for(temp = head->next; temp != head; temp = temp->next){ //loop through the rest of the list
+       //if the name entered equals name in list
+        if(temp->first == first){
+            if(temp->last == last){
+                del_ptr = temp;//pointer to temp
+                found++;
+}
+}
+}
+}
+if(found){ 
+    //link the list
+    del_ptr->prev->next = del_ptr->next;
+    del_ptr->next->prev = del_ptr->prev;
+    delete del_ptr;//delete specific element
+    
+    cout << "Player was removed" << endl;
+}
+else{ 
+    cout << "Player not found" << endl;
+  }
+}
+
+void deleteall(){  
+  player *deleted = head;//initalize deleted pointer to beginning of list
+  head->prev->next = 0;//head = 0
+//loop through list 
+  while(deleted){
+    head = head->next;//set head equal to head next to create room to delete
+    delete deleted;//delete the all the pointer values that is iterating through the list
+    deleted = head;//reinitalize deleted pointer to head
+    }
+}
+
+void display(){ 
+player *current;
+for(current = head->next; current != head; current = current->next){//loop through entire list
+if(current == head->next){//print first element Tom Brady
+    cout << head->first << " " << head->last << " ";
+    //print the years for each player
+    for(int i = 0; i < head->numWin; i++){
+    cout << head->year[i] << " ";
+    } 
+cout << endl;
+}//print the remaining players and years
+cout << current->first << " " << current->last << " ";
+for(int i = 0; i < current->numWin; i++){
+    cout << current->year[i] << " ";
+}
+cout << endl;
+}
+}
+
+int main(){
+    
+    char choice;
+    //populate function
+    populate();
+ 
+    do{
+    choice = menu();
+    switch(choice){
+        case '1':
+        display();
+        break;
+        case '2':
+        add();
+        break;
+        case '3':
+        search();
+        break;
+        case '4':
+        deleteplayer();
+        break;
+        default:
+        return 0;
+    }
+    
+    }while(choice != '5');
+
+    deleteall();
+
+    return 0;
+}
